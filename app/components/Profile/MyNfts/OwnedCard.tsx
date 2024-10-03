3c9b3f2365489eef777fb7d4566529b667ef0cde
Olagboye Seyi <akingbolaola@gmail.com>
2024-10-03 14:57:24 +0100

    Updated NFT navigation

--- app/components/Profile/MyNfts/OwnedCard.tsx
+++ app/components/Profile/MyNfts/OwnedCard.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import heart from "@/public/assets/heart.svg";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
@@ -8,54 +7,63 @@ import NftLoading from "../../Loader/NftLoading";
 import axios from "axios";
 import { NftResult } from "@/types";
 import { useRouter } from "next/navigation";
-import FavoriteButton from "./favourite-button"
-const OwnedCard = ({ isTrending, item  }: {
-  isTrending: boolean,
-  item: NftResult
+import Link from "next/link";
+import FavoriteButton from "./favourite-button";
+const OwnedCard = ({
+  isTrending,
+  item,
+}: {
+  isTrending: boolean;
+  item: NftResult;
 }) => {
   const [liked, setLiked] = useState(false);
   const [openOptions, setOpenOptions] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [retrievedNft, setRetrievedNft] = useState<any>(({
+  const [retrievedNft, setRetrievedNft] = useState<any>({
     image: heart,
-    name: "NFT not found"
-  }));
-  const navigate = useRouter();
-  const { contract } = useContract(item.token_address)
-  const { data: nft , isLoading, error } = useNFT(contract, item.token_id)
-  
+    name: "NFT not found",
+  });
 
-if(loading) return <NftLoading />
+  const { contract } = useContract(item.token_address);
+  const { data: nft, isLoading, error } = useNFT(contract, item.token_id);
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push(`/nft/${item.token_address}/${item.token_id}`);
+  };
 
-  
-  // const handleMint = () => {
-  //   navigate("/mint-nft");
-  // };
-  // const handleEdit = () => {
-  //   navigate("/edit-nft");
-  // };
+  if (loading) return <NftLoading />;
+
   return (
     // <div className="w-full box-border  lg:min-w-[300px] md:h-[350px] sm:w-[280px] lg:w-[304px] h-full bg-no-repeat bg-cover bg-blue-header rounded-[10px] md:rounded-[20px] p-3 flex flex-col items-start space-y-[12px] justify-between relative">
-    <div className="w-full overflow-hidden box-border md:h-[350px] h-full bg-blue-header rounded-[10px] p-1 md:rounded-[20px] flex flex-col  relative">
+    <>
+    <Link href={`/nft/${item.token_address}/${item.token_id}`}>
+    <div
+      className="w-full overflow-hidden box-border md:h-[350px] h-full bg-blue-header rounded-[10px] p-1 md:rounded-[20px] flex flex-col  relative"
+    >
       <div className="absolute left-2 top-2">
-       { Number(item.amount) > 1 && <span className="bg-black text-[14px] rounded-full p-1 text-green"> {`x${item.amount}`} </span>}
+        {Number(item.amount) > 1 && (
+          <span className="bg-black text-[14px] rounded-full p-1 text-green">
+            {" "}
+            {`x${item.amount}`}{" "}
+          </span>
+        )}
       </div>
-    <MediaRenderer 
-    height="233px"
-    width="100%"
-    style={{
-      backgroundColor: nft?.metadata.background_color || "black"
-    }}
-    className="w-full flex-[3]  rounded-[20px] object-cover "
-    src={nft?.metadata.image}
-    />
+      <MediaRenderer
+        height="233px"
+        width="100%"
+        style={{
+          backgroundColor: nft?.metadata.background_color || "black",
+        }}
+        className="w-full flex-[3]  rounded-[20px] object-cover "
+        src={nft?.metadata.image}
+      />
       <div className="flex flex-1 overflow-hidden justify-between items-center w-full mb-2 px-2">
-      <div >
-      <p className="text-[16px] semibold">{nft?.metadata.name}</p>
-        <div className="text-[#FAC744]  text-[14px] semibold leading-[22px]">
-          0.3 SPT
+        <div>
+          <p className="text-[16px] semibold">{nft?.metadata.name}</p>
+          <div className="text-[#FAC744]  text-[14px] semibold leading-[22px]">
+            0.3 SPT
+          </div>
         </div>
-      </div>
         <div />
         <div className="flex gap-x-2 items-center">
           {/* {liked ? (
@@ -74,6 +82,8 @@ if(loading) return <NftLoading />
         </div>
       </div>
     </div>
+    </Link>
+    </>
   );
 };
 
