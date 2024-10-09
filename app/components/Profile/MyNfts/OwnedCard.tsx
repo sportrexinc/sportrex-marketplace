6f9e9e7eb0b779322015ab69e897abdd4354c5cb
daniekeys <starlordflash@gmail.com>
2024-10-09 12:22:16 +0100

    pr for image splitings

--- app/components/Profile/MyNfts/OwnedCard.tsx
+++ app/components/Profile/MyNfts/OwnedCard.tsx
@@ -9,6 +9,7 @@ import { NftResult } from "@/types";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import FavoriteButton from "./favourite-button";
+
 const OwnedCard = ({
   isTrending,
   item,
@@ -36,37 +37,44 @@ const OwnedCard = ({
   return (
     // <div className="w-full box-border  lg:min-w-[300px] md:h-[350px] sm:w-[280px] lg:w-[304px] h-full bg-no-repeat bg-cover bg-blue-header rounded-[10px] md:rounded-[20px] p-3 flex flex-col items-start space-y-[12px] justify-between relative">
     <>
-    <Link href={`/nft/${item.token_address}/${item.token_id}`}>
-    <div
-      className="w-full overflow-hidden box-border md:h-[350px] h-full bg-blue-header rounded-[10px] p-1 md:rounded-[20px] flex flex-col  relative"
-    >
-      <div className="absolute left-2 top-2">
-        {Number(item.amount) > 1 && (
-          <span className="bg-black text-[14px] rounded-full p-1 text-green">
-            {" "}
-            {`x${item.amount}`}{" "}
-          </span>
-        )}
-      </div>
-      <MediaRenderer
-        height="233px"
-        width="100%"
-        style={{
-          backgroundColor: nft?.metadata.background_color || "black",
-        }}
-        className="w-full flex-[3]  rounded-[20px] object-cover "
-        src={nft?.metadata.image}
-      />
-      <div className="flex flex-1 overflow-hidden justify-between items-center w-full mb-2 px-2">
-        <div>
-          <p className="text-[16px] semibold">{nft?.metadata.name}</p>
-          <div className="text-[#FAC744]  text-[14px] semibold leading-[22px]">
-            0.3 SPT
-          </div>
+      <div className="w-full overflow-hidden box-border md:h-[350px] h-full bg-blue-header rounded-[10px] p-1 md:rounded-[20px] flex flex-col  relative">
+        <div className="absolute left-2 top-2">
+          {Number(item.amount) > 1 && (
+            <span className="bg-black text-[14px] rounded-full p-1 text-green">
+              {" "}
+              {`x${item.amount}`}{" "}
+            </span>
+          )}
         </div>
-        <div />
-        <div className="flex gap-x-2 items-center">
-          {/* {liked ? (
+        <Link
+          href={`/nft/${item.token_address}/${item.token_id}`}
+          className="w-full"
+        >
+          <MediaRenderer
+            height="233px"
+            width="100%"
+            style={{
+              backgroundColor: nft?.metadata.background_color || "black",
+            }}
+            className="w-full flex-[3]  rounded-[20px] object-cover "
+            src={nft?.metadata.image}
+          />
+        </Link>
+        <div className="flex flex-1 overflow-hidden justify-between items-center w-full mb-2 px-2">
+                  <Link 
+            href={`/nft/${item.token_address}/${item.token_id}`}>
+            
+            <p className="text-[16px] semibold" onClick={handleNavigate}>
+              {nft?.metadata.name}
+            </p>
+            <div className="text-[#FAC744]  text-[14px] semibold leading-[22px]">
+              0.3 SPT
+            </div>
+          </Link>
+          
+          <div />
+          <div className="flex gap-x-2 items-center">
+            {/* {liked ? (
             <AiFillHeart
               className="text-[24px] text-yellow"
               onClick={() => setLiked(false)}
@@ -77,12 +85,11 @@ const OwnedCard = ({
               onClick={() => setLiked(true)}
             />
           )} */}
-          <FavoriteButton item={item} />
-          {/* <span className="regular text-[#ABABAB]">0</span> */}
+            <FavoriteButton item={item} />
+            {/* <span className="regular text-[#ABABAB]">0</span> */}
+          </div>
         </div>
       </div>
-    </div>
-    </Link>
     </>
   );
 };
